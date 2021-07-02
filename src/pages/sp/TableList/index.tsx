752fab070f01df9d10dce97559add98ccca67532
fengyanjing <fengyanjing@digibms.com>
2021-07-02 14:34:07 +0800

    feat：去掉lint-staged。

--- src/pages/sp/TableList/index.tsx
+++ src/pages/sp/TableList/index.tsx
@@ -11,7 +11,6 @@ import ProDescriptions from '@ant-design/pro-descriptions';
 import type { FormValueType } from './components/UpdateForm';
 import UpdateForm from './components/UpdateForm';
 import { rule, addRule, updateRule, removeRule } from '@/services/api';
-import './index.less';
 
 /**
  * 添加节点
